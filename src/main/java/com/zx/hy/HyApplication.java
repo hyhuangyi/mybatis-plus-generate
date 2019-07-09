ca3cd186c2ccdbb72c1a4354b0bedb45bd5dab94
hyhuangyi <597505910@qq.com>
2019-07-09 17:01:31 +0800

    mybatisplus 代码生成器

---
+++ src/main/java/com/zx/hy/HyApplication.java
@@ -0,0 +1,13 @@
+package com.zx.hy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyApplication.class, args);
+    }
+
+}
